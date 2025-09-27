d24ca8f2496e5ca910358952e733f480e1b99ce3
Jviguy <jviguytwo2@gmail.com>
2025-09-27 15:24:01 -0400

    tryte semi, no tests and a lot of stuff that im rethinking lol

--- ternary/src/lib.rs
+++ ternary/src/lib.rs
@@ -1,16 +1,6 @@
-mod trit;
+pub mod trit;
+pub mod tryte;
+mod utils;
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use trit::Trit;
+pub use tryte::Tryte;
---
+++ ternary/src/utils.rs
@@ -0,0 +1,3 @@
+pub const fn pow3(exp: usize) -> i64 {
+    3_i64.pow(exp as u32)
+}
